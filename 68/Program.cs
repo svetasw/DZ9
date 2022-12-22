b16b7bfd81c6f05fc130f6988d5de77eed4c31f1
Sveta <svetasw_17@mail.ru>
2022-12-22 17:02:57 +1100

    68

---
+++ 68/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.WriteLine("This program solves Ackermann's function.");
+Console.WriteLine();
+int numberM = EnterUserData("Enter the start number M:");
+int numberN = EnterUserData("Enter the end number N:");
+if (numberM < 0 || numberN < 0)
+{
+    Console.WriteLine("Please enter positive numbers m and n.");
+    return;
+}
+int result = AckermannFunction(numberM, numberN);
+Console.WriteLine(result);
+
+int AckermannFunction(int numberM, int numberN)
+{
+    if (numberM == 0)
+    {
+        return numberN + 1;
+    }
+    else if (numberM > 0 && numberN == 0)
+    {
+        return AckermannFunction(numberM - 1, 1);
+    }
+    else if (numberM > 0 && numberN > 0)
+    {
+        return AckermannFunction(numberM - 1, AckermannFunction(numberM, numberN - 1));
+    }
+    return AckermannFunction(numberM, numberN);
+}
+
+int EnterUserData(string nameUserData)
+{
+    Console.Write($"{nameUserData}");
+    return Convert.ToInt32(Console.ReadLine());
+}
