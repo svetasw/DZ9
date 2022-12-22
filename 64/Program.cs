27e7dfceac7b894b428d5c636952d44d1e9dccad
Sveta <svetasw_17@mail.ru>
2022-12-22 17:02:15 +1100

    64

---
+++ 64/Program.cs
@@ -0,0 +1,17 @@
+﻿int GetNumber(string msg)
+{
+    Console.Write(msg);
+    int userNumber = Convert.ToInt32(Console.ReadLine());;
+    return userNumber;
+}
+
+string StringRecursion(int number)
+{
+   if (number == 1) return number.ToString();
+   
+   return number + " " + StringRecursion(number - 1);
+}
+
+int userNumber = GetNumber("Enter number: ");
+Console.WriteLine();
+Console.Write(StringRecursion(number: userNumber));
